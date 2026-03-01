e64b6fe1a0765bd22fd7026e646788966e794e4d
P.M.Ngugi <mngugi7@hotmail.com>
2026-03-01 18:25:09 +0300

    research/models.py

---
+++ research/.ipynb_checkpoints/models-checkpoint.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class LearningSession(models.Model):
+    title = models.CharField(max_length=255)
+
+    time_spent = models.FloatField()          # Ts
+    concept_links = models.FloatField()       # Cl
+    knowledge_gaps = models.FloatField()      # Kg
+    research_questions = models.FloatField()  # Rq
+
+    learning_efficiency = models.FloatField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
