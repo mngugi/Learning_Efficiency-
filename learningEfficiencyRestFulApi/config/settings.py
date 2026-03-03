56f60a2bdebde789ef4b7672fa9af3f6516d80f9
P.M.Ngugi <mngugi7@hotmail.com>
2026-03-03 15:20:26 +0300

    learningEfficiencyRestFulApi/config/settings.py

--- learningEfficiencyRestFulApi/config/settings.py
+++ learningEfficiencyRestFulApi/config/settings.py
@@ -102,6 +102,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ]
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
